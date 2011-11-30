e3dd9161356a84a5301de2019aa955c9b1af9322
yuanying <yuanying@fraction.jp>
2011-11-30 15:12:01 +0900

    Image#point に index を追加。

--- lib/image.rb
+++ lib/image.rb
@@ -4,7 +4,7 @@ class Image
 
   property :id,         Serial
   property :path,       FilePath
-  property :point,      Integer,    :default => 0
+  property :point,      Integer,    :default => 0, :index => true
   property :created_at, DateTime
 end
 
